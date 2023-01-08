e013f16fbd28b8b901d737909ff0e3455ba8e365
Raj Nagar <rajnagar0728@gmail.com>
2023-01-08 23:15:39 +0530

    q

--- vector.cpp
+++ vector.cpp
@@ -43,10 +43,103 @@ using namespace std;
 //     {
 //     cout<<y[j]<<"  ";
 //     }cout<<endl;
-//     cout<<"size : "<<y.size();
-//     cout<<"capacity : "<<y.capacity();
-//     y.resize(10);
-//     cout<<" \n size : "<<y.size()<<endl;
-//     cout<<"capacity : "<<y.capacity();
+//     // cout<<"size : "<<y.size();
+//     // cout<<"capacity : "<<y.capacity();
+//     // y.resize(10);
+//     // cout<<" \n size : "<<y.size()<<endl;
+//     // cout<<"capacity : "<<y.capacity();
 //     return 0;
 // }
+
+
+
+
+// void shortzeroandone(vector<int> &v)
+// {
+//     int count_zero=0;
+//     for(int cat:v)
+//     {
+//         if(cat==0)
+//         {
+//             count_zero++;
+//         }
+//         int x=v.size();
+//         for(int k =0;k<x;k++)
+//         {
+//             if(k<count_zero)
+//             {
+//                 v[k]=0;
+//             }else{
+//                 v[k]=1;
+//             }
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout<<"enter the value for loop run : ";
+//     cin>>n;
+//     vector<int> v;
+//     for(int i =0;i<n;i++)
+//     {
+//         int cat;
+//         cin>>cat;
+//         v.push_back(cat);
+//     }
+//     shortzeroandone(v);
+//     for(int j =0;j<n;j++)
+//     {
+//         cout<<v[j]<<"  ";
+//     }
+//     return 0;
+// }
+                                /*
+                                sort even and odd 
+                                */            
+    void sortbyparity(vector<int> &v)
+
+    {
+        int left_ptr=0;
+        int right_ptr=v.size()-1; 
+        while(left_ptr<right_ptr)
+        {
+            if(v[left_ptr]%2==1 && v[right_ptr]%2==0)
+            {
+                swap(v[left_ptr],v[right_ptr]);
+                left_ptr++;
+                right_ptr--;
+            }
+            if(v[left_ptr]%2==0)
+        {
+            left_ptr++;
+        }
+        if(v[right_ptr]%2==1)
+        {
+            right_ptr--;
+        }
+        }
+        
+    }
+    int main()
+    {
+    
+        vector<int> v;
+
+        for(int i =0;i<6;i++)
+        {
+            int ele;
+            cin>>ele;
+            v.push_back(ele);
+        }
+
+        sortbyparity(v);
+        
+        for(int i = 0;i<6;i++)
+        {
+            cout<<v[i]<<" ";
+        }cout<<endl;
+
+        return 0;
+    }
